16f055a0c5c4287d18125f6f0619cb8514e8b236
pritiYadav1 <115352601+pritiYadav1@users.noreply.github.com>
2022-11-04 15:12:04 +0530

    Update Question2.java

--- 4_November_2022/Question2.java
+++ 4_November_2022/Question2.java
@@ -1,17 +1,22 @@
+/*
+This is my QUE
+
+*/
+
 package Dipanshu;
 
 public class Question2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-demo();
+demo(); // Function calling
 	}
 	public static void demo()
 	{
 		int i,j;
-		for(i=1;i<=5;i++)
+		for(i=1;i<=5;i++) // for row value 
 		{
-			for(j=1;j<=2*i-1;j++)
+			for(j=1;j<=2*i-1;j++) // for start value 
 			{
 				System.out.print("*");
 			}
